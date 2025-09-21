258a3f5c590ca0ac68be17340c1d1d0f394ae7ae
vishal <karapevishal@gmail.com>
2025-09-21 19:12:24 +0530

    main | config changes added for redux and all related data

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,8 @@
-import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite'
 
-export default defineConfig({ plugins: [react()] })
+export default defineConfig({ plugins: [react()],
+  optimizeDeps: {
+    include: ['react', 'react-dom', 'react-router-dom', 'axios', '@reduxjs/toolkit', 'react-redux']
+  }
+})
