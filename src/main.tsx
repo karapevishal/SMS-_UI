c1ec6f529d6f01878284a3e70bc0122620f7df47
vishal <karapevishal@gmail.com>
2025-09-23 21:00:57 +0530

    test

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,10 @@
 import React from 'react'
 import { createRoot } from 'react-dom/client'
 import { Provider } from 'react-redux'
-import { BrowserRouter } from 'react-router-dom'
+import { HashRouter as BrowserRouter } from 'react-router-dom'
 import App from './App'
-import store from './store/store'
 import './index.css'
+import store from './store/store'
 
 createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
