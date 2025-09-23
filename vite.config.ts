4153dd5ccc1c7ae0e040faaf1b30be22b3ecf93d
vishal <karapevishal@gmail.com>
2025-09-23 20:50:40 +0530

    css-changed | deployement changes has been doone

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,9 @@
 import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
 
-export default defineConfig({ plugins: [react()],
+export default defineConfig({ 
+  plugins: [react()],
+  base: "/SMS-_UI/",
   optimizeDeps: {
     include: ['react', 'react-dom', 'react-router-dom', 'axios', '@reduxjs/toolkit', 'react-redux']
   }
